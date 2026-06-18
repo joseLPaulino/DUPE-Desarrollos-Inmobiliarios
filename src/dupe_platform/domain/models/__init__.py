e11bfd9cca5e00115b6782e0f80b788cb0d13466
Jose Paulino <jose.paulino@hcltech.com>
2026-06-18 13:42:35 -0400

    fix: clean imports, export all domain models, remove legacy stubs, pgvector compose

--- src/dupe_platform/domain/models/__init__.py
+++ src/dupe_platform/domain/models/__init__.py
@@ -1,15 +1,21 @@
 from .project import Project, Unit, ProjectStatus, ProjectType
 from .client import Client
 from .payment_plan import PaymentPlan, Installment, InstallmentStatus
-from .transaction import BankTransaction, ReconciliationMatch, ConfidenceLevel
-from .budget import Budget, Partida, PartidaExecution, TrafficLight
-from .notification import Notification, NotificationChannel, NotificationStatus
+from .payment_plan import EscalationLevel
+from .transaction import BankTransaction, ReconciliationMatch, ConfidenceLevel, ReconciliationStatus
+from .budget import Budget, Partida, PartidaExecution, TrafficLight, PartidaType
+from .notification import Notification, NotificationChannel, NotificationStatus, NotificationTrigger
 
 __all__ = [
+    # Project
     "Project", "Unit", "ProjectStatus", "ProjectType",
+    # Client
     "Client",
-    "PaymentPlan", "Installment", "InstallmentStatus",
-    "BankTransaction", "ReconciliationMatch", "ConfidenceLevel",
-    "Budget", "Partida", "PartidaExecution", "TrafficLight",
-    "Notification", "NotificationChannel", "NotificationStatus",
+    # Collections
+    "PaymentPlan", "Installment", "InstallmentStatus", "EscalationLevel",
+    # Finance
+    "BankTransaction", "ReconciliationMatch", "ConfidenceLevel", "ReconciliationStatus",
+    "Budget", "Partida", "PartidaExecution", "TrafficLight", "PartidaType",
+    # Notifications
+    "Notification", "NotificationChannel", "NotificationStatus", "NotificationTrigger",
 ]
